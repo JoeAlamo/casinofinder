f18c84a334760d3f879b5f84da09f2b6914192a7
Joe Alamo <joe.keilty+github@gmail.com>
2016-06-04 15:16:04 +0000

    Add minor style fixes to edit form

--- resources/views/admin/casino/edit.blade.php
+++ resources/views/admin/casino/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <h1>Add a Casino</h1>
+                <h1>Editing {{ $casino->name }}</h1>
                 <div class="panel panel-default">
                     {!! $casinoForm !!}
                 </div>
